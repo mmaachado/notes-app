4daad52b91f3c075c705f9f7c544b9ef592e6629
Marcelo Machado <mjoelmachadojr@gmail.com>
2022-06-24 12:32:29 -0300

    first commit
    created empty project

---
+++ Notes App/Notes App WatchKit Extension/Notes_AppApp.swift
@@ -0,0 +1,19 @@
+//
+//  Notes_AppApp.swift
+//  Notes App WatchKit Extension
+//
+//  Created by Marcelo Machado on 24/06/22.
+//
+
+import SwiftUI
+
+@main
+struct Notes_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            NavigationView {
+                ContentView()
+            }
+        }
+    }
+}
