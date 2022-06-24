2b30402e6b83edb01d280ff2a235256f7c3bc828
Marcelo Machado <mjoelmachadojr@gmail.com>
2022-06-24 15:52:14 -0300

    Settings View
    Settings view && multiples lines show feature

--- Notes App/Notes App WatchKit Extension/View/ContentView.swift
+++ Notes App/Notes App WatchKit Extension/View/ContentView.swift
@@ -8,6 +8,8 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @AppStorage("lineCount") var lineCount:Int = 1
   
     @State private var notes:[Note] = [Note]()
     @State private var text:String = ""
@@ -102,7 +104,7 @@ struct ContentView: View {
                                     .frame(width: 4)
                                     .foregroundColor(.accentColor)
                                 Text(notes[i].text)
-                                    .lineLimit(1)
+                                    .lineLimit(lineCount)
                                     .padding(.leading, 5)
                             }
                         } //: hstack
